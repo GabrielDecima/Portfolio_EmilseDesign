d1380398b4549614c6989bbcac85c74f70e665ff
Gabriel Decima <gabrieldecima1028@gmail.com>
2023-12-20 21:30:17 -0300

    add filter in card projects

--- src/components/pages/projects.jsx
+++ src/components/pages/projects.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Container from '../container/container';
 import styles from './projects.module.css';
 import card1 from '../../assets/cards/Trinidad.png';
@@ -10,10 +11,61 @@ import card6 from '../../assets/cards/card6.png';
 
 
 
+
+
 // eslint-disable-next-line react/prop-types
 const Projects = ({isMenuOpen}) => {
 
+    const Muestras = [
+        {
+            id: 1,
+            img: card1,
+            categoria: 'interactivas',
+            url: 'https://www.canva.com/design/DAFwCRCF1Qk/SNq_ovdVHp7-56-3hSyZsQ/view?website#2:hay-momentos-inolvidables-que-se-atesoran-en-el-coraz-n-para-siempre-por-esta-raz-n-quiero-que-compartas-conmigo-ste-d-a-tan-especial'
+        },
+        {
+            id: 2,
+            img: card2,
+            categoria: 'Imagenes',
+            url: 'https://www.canva.com/design/DAFwCRCF1Qk/SNq_ovdVHp7-56-3hSyZsQ/view?website#2:hay-momentos-inolvidables-que-se-atesoran-en-el-coraz-n-para-siempre-por-esta-raz-n-quiero-que-compartas-conmigo-ste-d-a-tan-especial'
+        },
+        {
+            id: 3,
+            img: card3,
+            categoria: 'Videos',
+            url: 'ejemplo.com'
+        },
+        {
+            id: 4,
+            img: card4,
+            categoria: 'interactivas',
+            url: 'ejemplo.com'
+        },
+        {
+            id: 5,
+            img: card5,
+            categoria: 'Imagenes',
+            url: 'ejemplo.com'
+        },
+        {
+            id: 6,
+            img: card6,
+            categoria: 'Imagenes',
+            url: 'ejemplo.com'
+        }
+
+        
+    ]
+
+    const [categoriaSeleccionada, setCategoriaSeleccionada] = useState('todos');
+
+    const filtrarPorCategoria = (categoria) => {
+      setCategoriaSeleccionada(categoria);
+    };
+  
+    const tarjetasFiltradas = categoriaSeleccionada === 'todos' ? Muestras : Muestras.filter((item) => item.categoria.toLowerCase() === categoriaSeleccionada.toLowerCase());
     return (
+
         <>
             <section className={styles.sectionProjects}>
            
@@ -21,26 +73,51 @@ const Projects = ({isMenuOpen}) => {
                 <div className={styles.containerNavigation}>
                     <ul className={styles.navigation}>
                         <li>
-                            <a href="#todos">Todos</a>
+                            <a onClick={() => filtrarPorCategoria('todos')} href="#Todos">Todos</a>
                         </li>
                         <li>
-                            <a href="#invitaciones">Interactivas</a>
+                            <a onClick={() => filtrarPorCategoria('interactivas')} href="#invitaciones">Interactivas</a>
                         </li>
                         <li>
-                            <a href="#fotos">Imagenes</a>
+                            <a onClick={() => filtrarPorCategoria('Imagenes')} href="#fotos">Imagenes</a>
                         </li>
                         <li>
-                            <a href="#Banners">Videos</a>
+                            <a onClick={() => filtrarPorCategoria('Videos')} href="#Banners">Videos</a>
                         </li>
 
                     </ul>
                 </div>
 
                 <div className={styles.containerCardsProject}>
-                    <div className={`${styles.cardProject} ${isMenuOpen ? styles.open : styles.close}`}>
+                    {tarjetasFiltradas.map((item) => {
+                            return (
+                                <div key={item.id} className={`${styles.cardProject} ${isMenuOpen ? styles.open : styles.close}`}>
+                                    <img src={item.img} alt="" />
+                                    <div className={styles.overlayText}><a href={item.url} target="_blank" rel="noopener noreferrer">Ver más</a></div>
+                                    <p>{item.categoria}</p>
+                                </div>
+                            )
+                        })
+                    }
+                    <div className={styles.prueba}>
+                       
+                    </div>
+                </div>
+            </Container>
+            </section>
+        </>
+    );
+}
+
+export default Projects;
+
+
+        {/* <div className={`${styles.cardProject} ${isMenuOpen ? styles.open : styles.close}`}>
                         <img src={card1} alt="" />
                         <div className={styles.overlayText}><a href="https://www.canva.com/design/DAFwCRCF1Qk/SNq_ovdVHp7-56-3hSyZsQ/view?website#2:hay-momentos-inolvidables-que-se-atesoran-en-el-coraz-n-para-siempre-por-esta-raz-n-quiero-que-compartas-conmigo-ste-d-a-tan-especial" target="_blank" rel="noopener noreferrer">Ver más</a></div>
                     </div>
+
+
                     <div className={`${styles.cardProject} ${isMenuOpen ? styles.open : styles.close}`}>
                         <img src={card2} alt="" />
                         <div className={styles.overlayText}>Ver más</div>
@@ -57,16 +134,10 @@ const Projects = ({isMenuOpen}) => {
                      <img src={card5} alt="" />
                      <div className={styles.overlayText}>Ver más</div>
                     </div>
+
+
+
                     <div className={`${styles.cardProject} ${isMenuOpen ? styles.open : styles.close}`}>
                         <img src={card6} alt="" />
                         <div className={styles.overlayText}>Ver más</div>
-                    </div>
-
-                </div>
-            </Container>
-            </section>
-        </>
-    );
-}
-
-export default Projects;
+                    </div> */}
